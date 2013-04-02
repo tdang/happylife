717845c332e3caa7dc242cf0004acbe94f52146c
Toan Dang <toan.s.dang@gmail.com>
2013-04-02 14:00:38 -0700

    finish ch10

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happylife::Application.config.secret_token = '157d3d7e24b27e3d456a3f2e5577ff5f0553d190ad5bae93a3ba52e5b0f132cac6a9d594b227331f82127cc077b723f743c40d631ea8d662707c7377c4e18458'
